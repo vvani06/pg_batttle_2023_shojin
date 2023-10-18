73d3181dd0c9adaa999da948822ba9487bf36732
daylight55 <jojomasanori3@gmail.com>
2023-10-19 00:19:50 +0900

    2020 ましゅまろ 難易度3 「距離感」: indexの記録方法を修正

--- 01_marshmallow/2020_3/main.rb
+++ 01_marshmallow/2020_3/main.rb
@@ -1,20 +1,28 @@
 #!/bin/env ruby
 
-n = gets.chomp.to_i
-a = gets.chomp.split.map(&:to_i)
+def main
+  n = gets.chomp.to_i
+  a = gets.chomp.split.map(&:to_i)
 
-between_counts = Array.new(n + 1, 0)
-a.each_with_index{ |ai, i|
-  last = a.rindex(ai)
-  next if i == last
-  between_num = a.rindex(ai) - i - 1
-  between_counts[ai] = between_num
-}
+  index_recorder = Hash.new{ |h, k| h[k] = [] }
+  between_counts = []
+  a.each_with_index{ |ai, i|
+    index_recorder[ai] << i
+    if index_recorder[ai].size == 2
+      between_counts << index_recorder[ai][1] - index_recorder[ai][0] - 1
+    end
+  }
 
-# 先頭のNo0は数える際に邪魔なため削除
-between_counts.shift
+  (2 * n - 1).times{|k|
+    puts between_counts.count{|i| i <= k }
+  }
+end
 
-# puts between_counts
-(2 * n - 1).times{|k|
-  puts between_counts.count{|i| i <= k }
-}
+main
+
+# lower_bound, upper_boundはいらんか
+# ref. https://atcoder.jp/contests/abc321/submissions/45823708
+# class Range
+#   def upper_bound; ac, wa = self.begin, self.end_open; while wa - ac > 1; if yield((wj = (ac + wa) / 2)); ac = wj else wa = wj end; end; yield(ac) ? ac : nil end
+#   def lower_bound; ac, wa = self.end_open, self.begin; while ac - wa > 1; if yield((wj = (ac + wa) / 2)); ac = wj else wa = wj end; end; yield(ac) ? ac : nil end
+# end
